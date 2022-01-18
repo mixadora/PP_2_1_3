69bf3873f1d2e49644782603182aee2d6e36b9a0
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:47:25 +0300

    2.1.3

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,15 @@
 package app.config;
 
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
 }
